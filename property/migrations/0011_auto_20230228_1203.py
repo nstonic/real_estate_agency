a123845d7ba410eff32e51a1f05f640ff1c3b7c1
nstonic <nstonic@gmail.com>
2023-03-01 09:14:38 +0400

    Use get_or_create() instead get()

--- property/migrations/0011_auto_20230228_1203.py
+++ property/migrations/0011_auto_20230228_1203.py
@@ -7,7 +7,11 @@ def link_owner_to_flat(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Owner = apps.get_model('property', 'Owner')
     for flat in Flat.objects.all().iterator():
-        Owner.objects.get(pure_phone=flat.owner_pure_phone).flats.add(flat)
+        Owner.objects.get_or_create(
+            full_name=flat.owner,
+            pure_phone=flat.owner_pure_phone,
+            defaults={'phonenumber': flat.owners_phonenumber}
+        )[0].flats.add(flat)
 
 
 class Migration(migrations.Migration):
